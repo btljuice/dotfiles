f238ebb1c82daa2e0c71e4aeb50c386328054439
btljuice <alexis.trudeau@gmail.com>
2017-10-06 12:59:35 -0400

    getMasterVolume detects muted volume

--- bin/getMasterVolume.sh
+++ bin/getMasterVolume.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-amixer sget Master | grep -E -m 1 -o '[0-9]+%'
+MUTE=`amixer sget Master | grep -E -o -m1 '(\[off\])|(\[on\])'`
+if [ "$MUTE" == "[off]" ];
+then
+    echo 'mute'
+else
+    amixer sget Master | grep -E -m 1 -o '[0-9]+%'
+fi
